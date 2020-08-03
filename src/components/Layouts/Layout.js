88d7932f08781c26afa3696ac193468ed095f680
mr-blankson <fat_tonyy@icloud.com>
2020-08-03 16:58:53 +0000

    imported-useMetadata-component-to-layout :octocat:

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -2,9 +2,15 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 // styles
-import { GlobalStyles, lightTheme, darkTheme } from "../../styles/Globalstyles";
+import { GlobalStyles, lightTheme, darkTheme } from "styles/Globalstyles";
+
+// hooks
+import { useMetaDataQuery } from "hooks/useMetaDataQuery";
 
 export default function Layout({ children }) {
+	const data = useMetaDataQuery();
+	console.log(data);
+
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<GlobalStyles />

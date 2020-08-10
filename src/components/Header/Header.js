54da048a1b5f0d0136a6c0ae4f09259c5f3f7eea
mr-blankson <fat_tonyy@icloud.com>
2020-08-10 15:43:46 +0000

    imported-hamburger-to-header :octocat:

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 
 // component
 import Menu from "../Menu/Menu";
-
+import Hamburger from "../Hamburger/Hamburger";
 //hooks
 import { useSiteConfigQuery } from "hooks/useSiteConfigQuery";
 
@@ -12,9 +12,11 @@ import { Wrapper, Logo } from "./HeaderStyles";
 
 export default function Header({ siteTitle = `` }) {
 	const siteConfig = useSiteConfigQuery();
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	return (
 		<Wrapper>
+			<Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 			<Menu items={siteConfig.menu} />
 			<Link to="/">
 				<Logo src={siteConfig.logo.publicURL} alt={siteTitle} />

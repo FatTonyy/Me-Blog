10fc161d98afe612cc13009fc5be5971b4d7271a
mr-blankson <fat_tonyy@icloud.com>
2020-08-13 14:20:53 +0000

    Imported-BGImage-and-useHeroQuery-to-hero-component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "gatsby";
+
+// components
+import BGImage from "../BGImage/BGImage";
+//hooks
+import { useHeroQuery } from "hooks/useHeroQuery";
+//styles
 
 export default function Hero() {
-	return <div>Hero</div>;
+	const { heroImage, heroImageText, heroImageBtnText, heroImageBtnLink } = useHeroQuery();
+	console.log(heroImage);
+	return (
+		<div>
+			<BGImage title="heroImage" fluid={heroImage.childImageSharp.fluid}>
+				<div>
+					<h2>{heroImageText}</h2>
+					<Link to={heroImageBtnLink}>
+						<button>{heroImageBtnText}</button>
+					</Link>
+				</div>
+			</BGImage>
+		</div>
+	);
 }

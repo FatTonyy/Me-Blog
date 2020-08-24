ee727bf0c59ff46f64c27c5d3919f58997e07f91
f.X Blanson <fat_tonyy@icloud.com>
2020-08-24 17:22:16 +0000

    functionality-for-blogpostCard

--- src/components/BlogPostCard/BlogPostCard.js
+++ src/components/BlogPostCard/BlogPostCard.js
@@ -1,5 +1,35 @@
 import React from "react";
+import Img from "gatsby-image";
+import { Link } from "gatsby";
 
-export default function BlogPostCard() {
-	return <div>BlogPostCard</div>;
+// icons
+import DateIcon from "../../images/calendar.svg";
+import TimeIcon from "../../images/watch.svg";
+
+export default function BlogPostCard({ slug, title, date, readingTime, excerpt, image }) {
+	return (
+		<CardWrapper>
+			<ImgWrapper>
+				<Img fluid={image} />
+			</ImgWrapper>
+
+			<TextWrapper>
+				<Link to={slug}>
+					<h2>{title}</h2>
+				</Link>
+				<StatWrapper>
+					<DateWrapper>
+						<img src={DateIcon} alt="date" />
+					</DateWrapper>
+					{date}
+					<ReadingTimeWrapper>
+						<img src={TimeIcon} alt="time" />
+						{readingTime}
+					</ReadingTimeWrapper>
+				</StatWrapper>
+
+				<p>{excerpt}</p>
+			</TextWrapper>
+		</CardWrapper>
+	);
 }

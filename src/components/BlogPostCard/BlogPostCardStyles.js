ca930d202c502fed40ded19c989531d44b4d1492
f.X Blanson <fat_tonyy@icloud.com>
2020-08-26 13:09:05 +0000

    added-little-twaeks-to-blogpostcardstyles

--- src/components/BlogPostCard/BlogPostCardStyles.js
+++ src/components/BlogPostCard/BlogPostCardStyles.js
@@ -41,18 +41,24 @@ With the imported named css from styled-components , you use it when you want to
 const sharedStyled = css`
 	display: flex;
 	align-items: center;
-	margin-right: 2rem;
+	margin-right: 0.5rem;
 	font-family: "Inter", sans-serif;
 	font-size: 0.8rem;
 
 	img {
-		margin: 0 1rem 0 0;
+		margin: 0 0.5rem 0 0;
 	}
 `;
 
 export const DateWrapper = styled.div`
 	/* using css from sharedStyles */
 	${sharedStyled}
+
+	img {
+		height: 2.25rem;
+		width: 2.25rem;
+		fill: currentColor;
+	}
 `;
 
 export const ReadingTimeWrapper = styled.div`

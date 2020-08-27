b6dc73a7244046b2472fa4a8f0667ed24d0a3953
f.X Blanson <fat_tonyy@icloud.com>
2020-08-27 18:47:02 +0000

    imported-modeButton-into-header-component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "gatsby";
 
 // component
 import Menu from "../Menu/Menu";
 import Hamburger from "../Hamburger/Hamburger";
 import MobileMenu from "../MobileMenu/MobileMenu";
+import ModeButton from "../ModeButton/ModeButton";
 //hooks
 import { useSiteConfigQuery } from "hooks/useSiteConfigQuery";
+import { ModeContext } from "../context/ModeProvider";
 
 // styles
 import { Wrapper, Logo } from "./HeaderStyles";
@@ -14,7 +16,7 @@ import { Wrapper, Logo } from "./HeaderStyles";
 export default function Header({ siteTitle = `` }) {
 	const siteConfig = useSiteConfigQuery();
 	const [menuOpen, setMenuOpen] = useState(false);
-
+	const [darkMode, setDarkMode] = useContext(ModeContext);
 	return (
 		<Wrapper>
 			<Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
@@ -23,7 +25,7 @@ export default function Header({ siteTitle = `` }) {
 			<Link to="/">
 				<Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
 			</Link>
-			switch button
+			<ModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
 		</Wrapper>
 	);
 }
